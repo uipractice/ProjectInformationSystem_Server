f0081cb4047a84c71a0fc46eb2b20a3aab1c756f
Sai Rajesh Paleti <spaleti@evoketechnologies.com>
2021-08-16 18:08:46 +0530

    PIS-Provide-Feedback-Mail: Provide feedback mail functionality

--- mails/feedbackMail.js
+++ mails/feedbackMail.js
@@ -43,15 +43,9 @@ const feedbackMail = (feedbackBody) => {
   // Step 3
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
-      res.json({
-        status: 'failed to send the mail',
-      });
       log('Filed to send, to see the detials uncomment below log');
       log('Error occured in sending the mail : ', err);
     } else {
-      res.json({
-        status: 'success',
-      });
       log('Mail Sent Successfully, to see the detials uncomment below log');
       log('Mail sent successfully', info);
     }
