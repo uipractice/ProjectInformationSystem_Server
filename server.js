7a5e8eb1bd1735a641475ee31ea27add3ccbdd7e
Sai Rajesh Paleti <spaleti@evoketechnologies.com>
2021-08-18 13:36:37 +0530

    Merge with latest

--- server.js
+++ server.js
@@ -1,74 +1,102 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const multer = require("multer");
-require("dotenv").config();
-let ClientInfo = require("./models/clientInfo.model");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const AWS = require('aws-sdk');
+require('dotenv').config();
+let ClientInfo = require('./models/clientInfo.model');
 
 const log = console.log;
-const clientInfoRouter = require("./routes/clientInfo");
- 
+const clientInfoRouter = require('./routes/clientInfo');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
- 
-const fileStorageEngine = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./uploadDoc");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "--" + file.originalname);
-  },
-});
 
-const upload = multer({ storage: fileStorageEngine });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-app.post("/health", (req, res) => {
-  res.send("Succss"); 
-});
-
-app.post("/multiple/:id", upload.array("fileName", 50), (req, res) => {
-  log("1 FileName : ", req.files[0].originalname); 
-  log("2 Date + FileName : ", req.files[0].filename); 
-  log("3 Path + Date + FileName : ", req.files[0].path); 
-  res.send("Files Upload is Successful"); 
-
-  ClientInfo.findById(req.params.id)
-  .then((clientInfo) => {
-    clientInfo.fileName = req.files[0].filename;
-    clientInfo.pathName = req.files[0].path;
-    clientInfo
-      .save()
-      .then(() => {
-        res.json("File path saved to DB")
-        log(res.data)
-      })
-      .catch((err) => res.status(400).json("Error: " + err));
-  })
-  .catch((err) => res.status(400).json("Error: " + err));
-
-});
-
-// app.get('/uploadDoc/:id', async(req, resp) => {
-//   ClientInfo.findById(req.params.id)
-//   .then((clientInfo) => {
-//     clientInfo.fileName = req.files[0].filename;
-//     clientInfo.pathName = req.files[0].path;
-//     clientInfo
-//       .save()
-//       .then(() => {
-//         res.json('File path saved to DB'); 
-//         log(res.data)
-//       })
-//       .catch((err) => res.status(400).json("Error: " + err));
-//   })
-//   .catch((err) => res.status(400).json("Error: " + err));
-
-//     let filePath = path.resolve(`./job_files/${authorized.rows[0].job_id}/${authorized.rows[0].milestone_id}/${downloadFile.rows[0].filename}`);
-//     resp.download(filePath, downloadFile.rows[0].filename, (err) => {
-//         if (err) console.log(err);
-//     });
+// const fileStorageEngine = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, './uploadDoc');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + '--' + file.originalname);
+//   },
 // });
+
+app.post('/health', (req, res) => {
+  res.send('Succss');
+});
+
+// const upload = multer({ storage: fileStorageEngine });
+
+// app.post('/multipleOld/:id', upload.array('fileName', 50), (req, res) => {
+//   const files = req.files || [];
+//   log('1 FileName : ', req.files[0].originalname);
+//   log('2 Date + FileName : ', req.files[0].filename);
+//   log('3 Path + Date + FileName : ', req.files[0].path);
+//   ClientInfo.findById(req.params.id)
+//     .then((clientInfo) => {
+//       clientInfo.fileName = req.files[0].filename;
+//       clientInfo.pathName = req.files[0].path;
+//       clientInfo
+//         .save()
+//         .then(() => {
+//           res.json('Files Upload is Successful and File path saved to DB');
+//           log(res.data);
+//         })
+//         .catch((err) => res.status(400).json('Error: ' + err));
+//     })
+//     .catch((err) => res.status(400).json('Error: ' + err));
+// });
+
+app.post('/multiple/:id', upload.array('fileName', 50), (req, res) => {
+  const files = req.files || [];
+  ClientInfo.findById(req.params.id).then((client) => {
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.ACCESS_KEY_ID,
+      secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    });
+    const params = {
+      Bucket: process.env.S3_BUCKET,
+      CreateBucketConfiguration: {
+        // Set your region here
+        LocationConstraint: 'ap-south-1',
+      },
+    };
+
+    const uploadedFiles = [];
+    for (let file of files) {
+      log('1 FileName : ', file.originalname);
+      const fileNameArray = file.originalname.split('.');
+      // Params for file Upload
+      params.Body = file.buffer;
+      params.Key = `${req.params.id}_${fileNameArray[0]}.${fileNameArray[1]}`;
+      params.ContentType = file.mimetype;
+      params.ContentDisposition = `attachment; filename=${file.originalname}`;
+      // Uploading files to the bucket
+      s3.upload(params, function (err, data) {
+        if (err) {
+          res.status(400).json('File upload failed Error: ' + err);
+          throw err;
+        }
+        console.log(`File uploaded successfully.`, data);
+        uploadedFiles.push(`${data.Key}`);
+        if (uploadedFiles.length === files.length) {
+          // Update the Db with the uploaded filenames
+          client.uploadedFiles = uploadedFiles;
+          client
+            .save()
+            .then(() => {
+              res.json('File Upload Successful and saved to DB');
+            })
+            .catch((err) => res.status(400).json('Error: ' + err));
+        }
+      });
+    }
+  });
+});
 
 // app.get("/download", function(req, res){
 //   res.download('./uploadDoc/1626894517317--Habbits.txt', '1626894517317--Habbits.txt');
@@ -83,7 +111,6 @@ mongoose.connect(uri, {
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-mongoose.connection.once("open", () => log("MongoDB Connection is successful"));
+mongoose.connection.once('open', () => log('MongoDB Connection is successful'));
 
-
-app.use("/clientInfo", clientInfoRouter);
+app.use('/clientInfo', clientInfoRouter);
