095a95025ff2593445f93cb79ce094f1dab372c6
Pavan Kumar Devaguptapu <pdevaguptapu@evoketechnologies.com>
2021-09-24 19:57:21 +0530

    bcrypt package changed to bcryptjs

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,5 @@
 var jwt = require("jsonwebtoken");
-var bcrypt = require("bcrypt");
+var bcrypt = require("bcryptjs");
 const User =require("../models/user.model");
 const config = require("../config/auth.config");
 
