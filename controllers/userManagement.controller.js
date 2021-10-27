31708a8c7f0b6662dde1c517864cde3fef13f7c9
Pavan Kumar Devaguptapu <pdevaguptapu@evoketechnologies.com>
2021-10-27 17:22:05 +0530

    change in return value

--- controllers/userManagement.controller.js
+++ controllers/userManagement.controller.js
@@ -4,9 +4,10 @@ const User =require("../models/user.model");
 const config = require("../config/auth.config");
 
  exports.users=(req,res)=>{
-        User.find({})
+        User.find()
         .then((user)=>{
-                return user;
+            console.log("user",user)
+                return res.json(user);
         })
         .catch((err) => res.status(400).json('Error: ' + err))
     }
