1035c85cc424bfefb3948523e0bd30684ea0ce89
Sai Rajesh Paleti <spaleti@evoketechnologies.com>
2021-11-17 19:22:28 +0530

    update functionality

--- controllers/userManagement.controller.js
+++ controllers/userManagement.controller.js
@@ -24,7 +24,7 @@ exports.createUser = (req, res) => {
       }
     })
     .catch((err) => {
-        return res.status(500).send({ message: 'internal server error' });;
+        return res.status(500).send({ message: 'internal server error' });
     });
 };
 
@@ -43,7 +43,7 @@ exports.deleteUser = (req, res) => {
         new: true,
       })
         .then((user)=>{
-            return res.status(200).send({ message: 'user updated successfully' });
+            return res.status(200).send({ message: 'user updated successfully', status: 'success' });
         })
       .catch((err) => {
           return res.status(500).send({ message: 'internal server error' });;
